0a91a28eb2a068ed7c979f3760d97ac80a8317be
kmiqbal19 <km.iqbal01@gmail.com>
2021-07-01 15:54:53 +0200

    Removed redux logger for production, kept only for development

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,8 +3,11 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../root-reducer";
-const middlewares = [logger];
+const middlewares = [];
 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
